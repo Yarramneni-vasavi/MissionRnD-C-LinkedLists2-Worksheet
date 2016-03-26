e2fe6a3441b265f0e02f59c944012b0fdff1e14c
Yarramneni-vasavi <Yarramneni-vasavi@missionrnd.com>
2016-03-25 19:10:55 -0700

    Submit Commit

--- src/reverseLinkedList.cpp
+++ src/reverseLinkedList.cpp
@@ -18,6 +18,48 @@ struct node {
 	struct node *next;
 };
 
-struct node * reverseLinkedList(struct node *head) {
-	return NULL;
+struct node * reverseLinkedList(struct node *head) 
+{
+	if (head == NULL)
+		return NULL;
+
+	struct node *p = head;
+	struct node *q = head->next;
+	struct node *q_trace = NULL;
+	struct node *temp1 = NULL;
+	
+	if (q != NULL)
+	{
+		q_trace = q->next;
+	}
+
+	while (q_trace != NULL)
+	{
+		p->next = NULL;
+		q->next = NULL;
+		q->next = p;
+		p->next = temp1;
+		temp1 = q;
+		p = q_trace;
+		q = q_trace->next;
+		if (q_trace->next != NULL)
+			q_trace = q_trace->next->next;
+		else
+			q_trace = NULL;
+	}
+	if (q_trace == NULL && q!=NULL)
+	{
+		p->next = NULL;
+		q->next = NULL;
+		q->next = p;
+		p->next = temp1;
+		temp1 = q;
+	}
+	if (p->next == NULL && q == NULL)
+	{
+		p->next = temp1;
+		temp1 = p;
+	}
+	head = temp1;
+	return head;
 }
