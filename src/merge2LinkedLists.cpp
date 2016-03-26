e9cec1ace7cbc07f1a9e0ecb09ccfa671a1b9211
Yarramneni-vasavi <Yarramneni-vasavi@missionrnd.com>
2016-03-25 21:25:44 -0700

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -18,6 +18,29 @@ struct node {
 	struct node *next;
 };
 
-struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+struct node * merge2LinkedLists(struct node *head1, struct node *head2) 
+{
+	if (head1 == NULL && head2 == NULL)
+		return NULL;
+
+	struct node *MergedSortedList = NULL;
+	if (head1 == NULL)
+	{
+		return head2;
+	}
+	else if (head2 == NULL)
+	{
+		return head1;
+	}
+	if (head1->num <= head2->num)
+	{
+		MergedSortedList = head1;
+		MergedSortedList->next = merge2LinkedLists(head1->next, head2);
+	}
+	else
+	{
+		MergedSortedList = head2;
+		MergedSortedList->next = merge2LinkedLists(head1, head2->next);
+	}
+	return MergedSortedList;
 }
